4a08c0937eb4febfedcc9a3661567c4463da4c40
Anton Pomozov <pomozoff@mail.ru>
2020-04-15 23:23:25 +0300

    Initial Commit

---
+++ LargeImages/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LargeImages
+//
+//  Created by Anton Pomozov on 15.04.2020.
+//  Copyright © 2020 Akademon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
